1c9b52456d2ffab386d6d4c13bd157f1f3749914
danniranderis <danni@danniranderis.dk>
2018-05-25 09:44:00 +0200

    Initial setting up "minimal viable product" for a php-site and running all the docker micro-services

---
+++ web/src/index.php
@@ -0,0 +1,17 @@
+<?php
+
+# Include the base header
+include_once('base_header.php');
+
+?>
+
+<h1>Dette er en hjemmeside</h1>
+<p>Og dette er en paragraf.</p>
+
+<?php
+
+# Include the base footer
+include_once('base_footer.php');
+
+
+?>
