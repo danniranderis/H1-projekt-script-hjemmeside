07db6ce57bf8eb0b1cb2f37ac59cdc38a7f4bea8
danniranderis <danni@danniranderis.dk>
2018-05-28 12:29:52 +0200

    Add jQuery and custom css for the question-answers.

--- web/src/base_header.php
+++ web/src/base_header.php
@@ -2,6 +2,10 @@
 <head>
     <meta charset="UTF-8">
     <title>H1 Projekthjemmeside</title>
+
+    <link rel="stylesheet" href="custom_styles.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 
 <body>
