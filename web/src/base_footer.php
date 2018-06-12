f154c5590029ba7fb19a614c363de84e5df310be
danniranderis <danni@danniranderis.dk>
2018-06-12 21:45:43 +0200

    Setting up mysql connection with example credentials.ini-file

--- web/src/base_footer.php
+++ web/src/base_footer.php
@@ -1,2 +1,7 @@
 </body>
 </html>
+
+<?php
+// Always make sure that the db-connection is closed after the page is done
+$db->close();
+?>
--- web/src/global.php
+++ web/src/global.php
@@ -3,6 +3,18 @@
  * Initial global settings included on every page
  **/
 
+// Set the absolute path for the project
+$absolute_path = realpath(dirname(__FILE__)) . '/';
+
 // Set the locale/timezone to danish
 setlocale(LC_ALL, 'da_DK.utf8');
 date_default_timezone_set('Europe/Copenhagen');
+
+// Set up the connection to the database
+$db_conf = parse_ini_file($absolute_path.'db_credentials.ini');
+$db = new mysqli($db_conf['host'], $db_conf['user'], $db_conf['password'], $db_conf['database']);
+
+// Test the db-connection
+if ($db->connect_error) {
+    die('Kan ikke forbinde til databasen. Fejl: (' . $db->connect_errno . ') ' . $db->connect_error);
+}
